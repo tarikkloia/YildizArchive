9c257390bcb668dbb9e711b8752b2357bb710629
tarikkloia <tarik@kloai.com>
2025-10-10 08:42:10 +0300

    Revert "clean"
    This reverts commit 3bf1970aa73bc70a3ab8dff922d68fb2fc6ff3b0.

--- main.go
+++ main.go
@@ -100,7 +100,6 @@ func NewFileLogger(name string, i int, parallel int, tm string, lb int, up int, 
 		return nil, err
 	}
 	logs.File = f.Name()
-
 	return &FileLogger{
 		tmpFile:  f,
 		i:        i,
